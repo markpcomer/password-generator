9f07ee75d8b0c4fe5cb47d6cc7d76f3bb5caa761
markpcomer <markpcomer@gmail.com>
2024-12-30 22:23:15 -0700

    Refactoring & reorganizing functions.

--- script.js
+++ script.js
@@ -1,65 +1,56 @@
-// 1  Issue prompts when button is pressed.
-// 2  Validate/Test Prompt Inputs.
-// 3  Generate Password.
-// 4  Display Generated Password in Alert Window.
+
 
 var generateBtn = document.querySelector("#generate");
-var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 
-function generatePassword(passwordLength){
-  var password = " ";
+function checkPassLength(passwordLength) {
+  
+}
+
+function generatePassword(passwordLength, characters) {
+  var password = "";
   for(var i = 0; i < passwordLength; i++){
     password += characters.charAt(Math.floor(Math.random() * characters.length));
   }
   return password;
-}
+};
+
+function passTrue(passwordLength) {
+  return passwordLength >= 8 && passwordLength <= 128;
+};
 
 // Write password to the #password input
 function writePassword() {
   var passwordText = document.querySelector("#password");
-  var passwordLength = prompt("Choose Password Length: 8 - 128.");
+  var passwordLength = prompt("Choose Password Length: 8 - 128 characters.");
+
+  // Validate password length
+  if (passwordLength === null || !passTrue(passwordLength)) {
+    alert("Password length must be between 8 and 128 characters.");
+    return;
+  }
+
   var lowercase = confirm("Hit Okay If You Would Like Lowercase Letters in Your Password.");
   var uppercase = confirm("Hit Okay If You Would Like Uppercase Letters in Your Password.");
   var numbers = confirm("Hit Okay If You Would Like Numbers in Your Password.");
   var specialChars = confirm("Hit Okay If You Would Like Special Characters in Your Password.");
 
-  var passTrue = function(passwordLength){
-    if (passwordLength >= 8 && passwordLength <= 128) {
-      return true;
-    } else {
-      return false;
-    }
+  var characters = "";
+  if (lowercase) characters += "abcdefghijklmnopqrstuvwxyz";
+  if (uppercase) characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (numbers) characters += "1234567890";
+  if (specialChars) characters += "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+
+  if (!characters) {
+    alert("Please select at least one character type.");
+    return;
   }
 
-    if (!lowercase) {
-      characters = characters.replace("abcdefghijklmnopqrstuvwxyz", " "); 
-  }
-  
-  
-    if (!uppercase) {
-      characters = characters.replace("ABCDEFGHIJKLMNOPQRSTUVWXYZ", " ");
-    }
-
-    if (!numbers) {
-       characters = characters.replace("123456789", " ");
-    }
-
-    if (!specialChars) {
-       characters = characters.replace("!#$%&'()*+,-./:;<=>?@[]^_`{|}~", " ");
-    }
-    var password = generatePassword(passwordLength);
-    
-
-  if ((passTrue === false) || ((uppercase === false ) && (lowercase === false) && 
-  (numbers === false) && (specialChars === false))){
-    alert("Please Start Over.")
-    } else {
-        alert(generatePassword(passwordLength, lowercase, uppercase, specialChars));
-    }
+  var password = generatePassword(passwordLength, characters);
   passwordText.value = password;
-  return password;
-    
-}
+  alert("Your generated password: " + password);
+
+  };
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
